b0b77fde40ea67c0981bc926427ad3393df15436
Pavel Bystritsky <pavel.bystritskyi@boldcommerce.com>
2024-07-15 11:47:45 +0200

    INTER-3709: Render Bold Payments

---
+++ Model/Payment/Gateway/Command/VoidPayment.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace Bold\CheckoutPaymentBooster\Model\Payment\Gateway\Command;
+
+use Bold\Checkout\Model\Order\SetIsDelayedCapture;
+use Bold\Checkout\Model\Payment\Gateway\Service;
+use Exception;
+use Magento\Payment\Gateway\CommandInterface;
+
+/**
+ * Void bold order payment.
+ */
+class VoidPayment implements CommandInterface
+{
+    /**
+     * @var Service
+     */
+    private $gatewayService;
+
+    /**
+     * @var SetIsDelayedCapture
+     */
+    private $setIsDelayedCapture;
+
+    /**
+     * @param Service $gatewayService
+     * @param SetIsDelayedCapture $setIsDelayedCapture
+     */
+    public function __construct(
+        Service $gatewayService,
+        SetIsDelayedCapture $setIsDelayedCapture
+    ) {
+        $this->gatewayService = $gatewayService;
+        $this->setIsDelayedCapture = $setIsDelayedCapture;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws Exception
+     */
+    public function execute(array $commandSubject): void
+    {
+        $paymentDataObject = $commandSubject['payment'];
+        $payment = $paymentDataObject->getPayment();
+        $this->setIsDelayedCapture->set($payment->getOrder());
+        $this->gatewayService->cancel($payment->getOrder(), Service::VOID);
+    }
+}

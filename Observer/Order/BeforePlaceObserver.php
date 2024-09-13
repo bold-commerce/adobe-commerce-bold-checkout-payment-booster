d44a1ab2075fafe18f1ff2f968857f609ffb492b
Nickolas Malovanets <nickolas.malovanets@boldcommerce.com>
2024-09-13 10:45:18 +0300

    Add credit card info to Magento payment from authorization full response.

--- Observer/Order/BeforePlaceObserver.php
+++ Observer/Order/BeforePlaceObserver.php
@@ -1,5 +1,4 @@
 <?php
-
 declare(strict_types=1);
 
 namespace Bold\CheckoutPaymentBooster\Observer\Order;
@@ -11,7 +10,8 @@ use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Model\Order\Payment\Transaction;
 
 /**
  * Authorize Bold payments before placing order.
@@ -22,11 +22,6 @@ class BeforePlaceObserver implements ObserverInterface
      * @var Authorize
      */
     private $authorize;
-
-    /**
-     * @var CartRepositoryInterface
-     */
-    private $cartRepository;
 
     /**
      * @var Session
@@ -40,18 +35,15 @@ class BeforePlaceObserver implements ObserverInterface
 
     /**
      * @param Authorize $authorize
-     * @param CartRepositoryInterface $cartRepository
      * @param Session $checkoutSession
      * @param CheckPaymentMethod $checkPaymentMethod
      */
     public function __construct(
         Authorize $authorize,
-        CartRepositoryInterface $cartRepository,
         Session $checkoutSession,
         CheckPaymentMethod $checkPaymentMethod
     ) {
         $this->authorize = $authorize;
-        $this->cartRepository = $cartRepository;
         $this->checkoutSession = $checkoutSession;
         $this->checkPaymentMethod = $checkPaymentMethod;
     }
@@ -72,6 +64,39 @@ class BeforePlaceObserver implements ObserverInterface
         }
         $publicOrderId = $this->checkoutSession->getBoldCheckoutData()['data']['public_order_id'] ?? '';
         $websiteId = (int)$order->getStore()->getWebsiteId();
-        $this->authorize->execute($publicOrderId, $websiteId);
+        $response = $this->authorize->execute($publicOrderId, $websiteId);
+        $this->saveTransaction($order, $response['data'] ?? []);
+    }
+
+    /**
+     * Add Bold transaction data to order payment.
+     *
+     * @param OrderInterface $order
+     * @param array $transactionData
+     * @return void
+     */
+    private function saveTransaction(OrderInterface $order, array $transactionData)
+    {
+        if (!isset($transactionData['transactions'][0]['transaction_id'])) {
+            return;
+        }
+        $order->getPayment()->setTransactionId($transactionData['transactions'][0]['transaction_id']);
+        $order->getPayment()->setIsTransactionClosed(0);
+        $order->getPayment()->addTransaction(Transaction::TYPE_AUTH);
+        $cardDetails = $transactionData['transactions'][0]['tender_details'] ?? null;
+        if (!$cardDetails) {
+            return;
+        }
+        $brand = $cardDetails['brand'] ?? '';
+        $lastFour = $cardDetails['last_four'] ?? '';
+        if (!$lastFour && isset($cardDetails['line_text'])) {
+            preg_match('/\b(\d{4})\b(?=\s*\(Transaction ID)/', $cardDetails['line_text'], $matches);
+            $lastFour = $matches[1] ?? '';
+        }
+        $order->getPayment()->setCcType($brand);
+        $order->getPayment()->setCcLast4($lastFour);
+        if (!$lastFour && isset($cardDetails['line_text'])) {
+            $order->getPayment()->setAdditionalInformation('tender_details', $cardDetails['line_text']);
+        }
     }
 }

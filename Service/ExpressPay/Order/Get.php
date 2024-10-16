27e59026e9945e05de4f7ec284ccbd22738b75f2
Joseph Leedy <joseph.leedy@boldcommerce.com>
2024-10-16 08:28:49 -0600

    Rename Get Order API parameter to be generic (CHK-5237)

--- Service/ExpressPay/Order/Get.php
+++ Service/ExpressPay/Order/Get.php
@@ -36,7 +36,7 @@ class Get
     }
 
     /**
-     * @param string $paypalOrderId
+     * @param string $orderId
      * @param string $gatewayId
      * @return array
      * @phpstan-return array{
@@ -64,10 +64,10 @@ class Get
      * }
      * @throws \Magento\Framework\Exception\LocalizedException
      */
-    public function execute($paypalOrderId, $gatewayId): array
+    public function execute($orderId, $gatewayId): array
     {
         $websiteId = (int)$this->storeManager->getStore()->getWebsiteId();
-        $uri = "checkout/orders/{{shopId}}/wallet_pay/$paypalOrderId?gateway_id=$gatewayId";
+        $uri = "checkout/orders/{{shopId}}/wallet_pay/$orderId?gateway_id=$gatewayId";
 
         try {
             $result = $this->httpClient->get($websiteId, $uri);

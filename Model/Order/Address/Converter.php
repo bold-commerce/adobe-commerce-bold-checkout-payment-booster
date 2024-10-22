8c6c01a5d746189ff95793ca1ebeb22ec01e5d4d
Nickolas Malovanets <nickolas.malovanets@boldcommerce.com>
2024-10-22 23:17:02 +0300

    Send country and region names in en_Us locale.

--- Model/Order/Address/Converter.php
+++ Model/Order/Address/Converter.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Bold\CheckoutPaymentBooster\Model\Order\Address;
 
 use Magento\Directory\Model\CountryFactory;
+use Magento\Directory\Model\RegionFactory;
+use Magento\Framework\Locale\ResolverInterface;
 use Magento\Sales\Api\Data\OrderAddressInterface;
 
 /**
@@ -17,9 +19,19 @@ class Converter
      */
     private $countryFactory;
 
-    public function __construct(CountryFactory $countryFactory)
+    /**
+     * @var RegionFactory
+     */
+    private $regionFactory;
+
+    /**
+     * @param CountryFactory $countryFactory
+     * @param ResolverInterface $resolver
+     */
+    public function __construct(CountryFactory $countryFactory, RegionFactory $regionFactory)
     {
         $this->countryFactory = $countryFactory;
+        $this->regionFactory = $regionFactory;
     }
 
     /**
@@ -30,18 +42,19 @@ class Converter
      */
     public function convert(OrderAddressInterface $address): array
     {
+        $region = $this->regionFactory->create()->load($address->getRegionId());
         $country = $this->countryFactory->create()->loadByCode($address->getCountryId());
         return [
             'id' => (int)$address->getId() ?: null,
             'business_name' => (string)$address->getCompany(),
             'country_code' => (string)$address->getCountryId(),
-            'country' => (string)$country->getName(),
+            'country' => (string)$country->getName('en_US'),
             'city' => (string)$address->getCity(),
             'first_name' => (string)$address->getFirstname(),
             'last_name' => (string)$address->getLastname(),
             'phone_number' => (string)$address->getTelephone(),
             'postal_code' => (string)$address->getPostcode(),
-            'province' => (string)$address->getRegion(),
+            'province' => (string)$region->getDefaultName(),
             'province_code' => (string)$address->getRegionCode(),
             'address_line_1' => (string)($address->getStreet()[0] ?? ''),
             'address_line_2' => (string)($address->getStreet()[1] ?? ''),

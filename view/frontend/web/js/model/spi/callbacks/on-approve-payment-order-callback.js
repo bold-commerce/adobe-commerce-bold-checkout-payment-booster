b801631940bd976017559d7c8e0554e164f930f8
Nickolas Malovanets <nickolas.malovanets@boldcommerce.com>
2024-10-24 17:01:58 +0300

    M2: As a brand with Braintree Wallet pays enabled, I am a offered these wallet pays at the payment step.

--- view/frontend/web/js/model/spi/callbacks/on-approve-payment-order-callback.js
+++ view/frontend/web/js/model/spi/callbacks/on-approve-payment-order-callback.js
@@ -1,22 +1,63 @@
 define(
     [
-        'Bold_CheckoutPaymentBooster/js/action/express-pay/place-order-action'
+        'uiRegistry',
+        'jquery',
+        'Magento_Checkout/js/model/quote',
+        'Magento_Checkout/js/action/place-order',
+        'Magento_Checkout/js/action/redirect-on-success',
+        'Bold_CheckoutPaymentBooster/js/action/express-pay/process-ppcp-order-action',
+        'Bold_CheckoutPaymentBooster/js/action/express-pay/process-braintree-order-action',
+        'Bold_CheckoutPaymentBooster/js/action/express-pay/save-shipping-information-action',
     ],
     function (
-        placeOrderAction
+        registry,
+        $,
+        quote,
+        placeOrderAction,
+        redirectOnSuccessAction,
+        processPpcpOrderAction,
+        processBraintreeOrderAction,
+        saveShippingInformationAction
     ) {
         'use strict';
 
         /**
-         * Callback function for approving payment order.
+         * Place express-order action.
          *
          * @param {string} paymentType
          * @param {{}} paymentInformation
-         * @param {{}} paymentPayload
+         * @param {{}} paymentApprovalData
          * @return {Promise}
          */
-        return async function (paymentType, paymentInformation, paymentPayload) {
-            return placeOrderAction(paymentType, paymentInformation, paymentPayload);
+        return async function (paymentType, paymentInformation, paymentApprovalData) {
+            if (paymentApprovalData === null) {
+                console.error('Express Pay payment data is not set.');
+                return;
+            }
+            const paymentMethodData = {
+                method: 'bold',
+            };
+            if (paymentType === 'ppcp') {
+                paymentMethodData['additional_data'] = {
+                    order_id: paymentApprovalData?.payment_data.order_id
+                };
+                await processPpcpOrderAction(paymentApprovalData);
+            } else {
+                await processBraintreeOrderAction(paymentInformation, paymentApprovalData);
+            }
+            try {
+                await saveShippingInformationAction(true);
+            } catch (error) {
+                console.error('Could not save shipping information for Express Pay order.', error);
+                return;
+            }
+            const messageContainer = registry.get('checkout.errors').messageContainer;
+            $.when(placeOrderAction(paymentMethodData, messageContainer))
+                .done(
+                    function () {
+                        redirectOnSuccessAction.execute();
+                    }
+                );
         };
     }
 );

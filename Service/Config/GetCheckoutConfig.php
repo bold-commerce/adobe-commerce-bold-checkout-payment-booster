42fe71bdf4d77b39e60bc27ec5bc5facf787c229
nicole.norman <nicole.norman@boldcommerce.com>
2024-11-19 09:33:29 -0600

    [CHK-4614] Adding Express Pay modal to the PDP Adding before cart add event to clear cart for quick purchase Adding support for SDK onClick event to add item to cart on pdp Adding page source argument to express pay xml to facilitate above add Adding functionality to checkout without a quote active Adding Endpoint to fetch Checkout Config to fill the window object Adding support to webapi_rest to fetch default config

---
+++ Service/Config/GetCheckoutConfig.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bold\CheckoutPaymentBooster\Service\Config;
+
+use Bold\CheckoutPaymentBooster\Api\Config\GetCheckoutConfigInterface;
+use Magento\Checkout\Model\CompositeConfigProvider;
+use Magento\Framework\Serialize\SerializerInterface;
+
+/**
+ * Hydrate Bold order from Magento quote.
+ */
+class GetCheckoutConfig implements GetCheckoutConfigInterface
+{
+    /**
+     * @var CompositeConfigProvider
+     */
+    private $configProvider;
+
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+
+    public function __construct(
+        CompositeConfigProvider $configProvider,
+        SerializerInterface $serializer
+    ) {
+        $this->configProvider = $configProvider;
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * Gets Current Checkout Configuration.
+     *
+     * @return string
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function getCheckoutConfig() {
+        $jsLayout = $this->configProvider->getConfig();
+        $config = $this->serializer->serialize($jsLayout);
+
+        return $config;
+    }
+}

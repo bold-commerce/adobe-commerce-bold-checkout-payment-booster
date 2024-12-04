d82b99194492c5c93c8d1840a7498d2d1fd77475
nicole.norman <nicole.norman@boldcommerce.com>
2024-12-04 16:41:33 -0600

    Updates to callbacks for cart & mini-cart express pay

--- ViewModel/ExpressPay.php
+++ ViewModel/ExpressPay.php
@@ -4,17 +4,13 @@ namespace Bold\CheckoutPaymentBooster\ViewModel;
 
 use Bold\CheckoutPaymentBooster\Model\CheckoutData;
 use Magento\Checkout\Model\CompositeConfigProvider;
-use Magento\Framework\Event\ManagerInterface;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Serialize\SerializerInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Magento\Checkout\Model\Session;
 use Magento\Store\Model\StoreManagerInterface;
 use Bold\CheckoutPaymentBooster\Model\Config;
-use Magento\Directory\Model\AllowedCountries;
-use Magento\Directory\Model\Country;
 use Magento\Directory\Model\ResourceModel\Country\CollectionFactory;
-use Magento\Config\Model\Config\Source\Nooptreq as NooptreqSource;
 use Bold\CheckoutPaymentBooster\UI\PaymentBoosterConfigProvider;
 
 class ExpressPay implements ArgumentInterface
@@ -22,25 +18,22 @@ class ExpressPay implements ArgumentInterface
     /**
      * @var CompositeConfigProvider
      */
-    protected $configProvider;
+    private $configProvider;
 
     /**
      * @var SerializerInterface
      */
-    protected $serializer;
+    private $serializer;
 
     /**
      * @var Session
      */
-    protected $checkoutSession;
-    /**
-     * @var ManagerInterface
-     */
-    protected $eventManager;
+    private $checkoutSession;
+
     /**
      * @var StoreManagerInterface
      */
-    protected $storeManager;
+    private $storeManager;
 
     /**
      * @var CheckoutData
@@ -50,54 +43,33 @@ class ExpressPay implements ArgumentInterface
     /**
      * @var Config
      */
-    protected $config;
+    private $config;
 
     /**
-     * @var AllowedCountries
+     * @var PaymentBoosterConfigProvider
      */
-    private $allowedCountries;
-
-    /**
-     * @var CollectionFactory
-     */
-    private $collectionFactory;
-
-    /**
-     * @var array
-     */
-    protected $countries = [];
+    private $paymentBoosterConfigProvider;
 
     /**
      * @var array
      */
     protected $jsLayout = [];
 
-    /**
-     * @var PaymentBoosterConfigProvider
-     */
-    protected $paymentBoosterConfigProvider;
-
     public function __construct(
         CompositeConfigProvider $configProvider,
         SerializerInterface $serializer,
         Session $checkoutSession,
-        ManagerInterface $eventManager,
         StoreManagerInterface $storeManager,
         CheckoutData $checkoutData,
         Config $config,
-        AllowedCountries $allowedCountries,
-        CollectionFactory $collectionFactory,
-        PaymentBoosterConfigProvider $paymentBoosterConfigProvider,
+        PaymentBoosterConfigProvider $paymentBoosterConfigProvider
     ) {
         $this->configProvider = $configProvider;
         $this->serializer = $serializer;
         $this->checkoutSession = $checkoutSession;
-        $this->eventManager = $eventManager;
         $this->storeManager = $storeManager;
         $this->checkoutData = $checkoutData;
         $this->config = $config;
-        $this->allowedCountries = $allowedCountries;
-        $this->collectionFactory = $collectionFactory;
         $this->paymentBoosterConfigProvider = $paymentBoosterConfigProvider;
     }
 
@@ -107,28 +79,25 @@ class ExpressPay implements ArgumentInterface
         return $this->serializer->serialize($this->jsLayout);
     }
 
-    /**
-     * @return bool
-     * @throws NoSuchEntityException
-     */
-    public function isCartWalletPayEnabled()
+    public function isCartWalletPayEnabled(): bool
     {
         $websiteId = $this->storeManager->getStore()->getWebsiteId();
         return $this->config->isCartWalletPayEnabled($websiteId);
     }
 
-    /**
-     * @param $websiteId
-     * @return bool
-     */
-    public function isProductWalletPayEnabled($websiteId)
+    public function isProductWalletPayEnabled($websiteId): bool
     {
         return $this->config->isProductWalletPayEnabled($websiteId);
     }
 
+    public function hasActiveQuote(): bool
+    {
+        $quote = $this->checkoutSession->getQuote();
+        return $quote->getId() !== null;
+    }
+
     public function initConfig(): array
     {
-        
         $this->checkoutData->initCheckoutData();
         return $this->paymentBoosterConfigProvider->getConfig();
     }

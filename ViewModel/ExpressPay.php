bc04fbe27d52f70bc84792cf49dbc4293ba7c5ca
Joseph Leedy <joseph.leedy@boldcommerce.com>
2024-12-12 14:47:22 -0700

    Remove disused method for initializing Bold config (CHK-4612)

--- ViewModel/ExpressPay.php
+++ ViewModel/ExpressPay.php
@@ -113,43 +113,4 @@ class ExpressPay implements ArgumentInterface
         $quote = $this->checkoutSession->getQuote();
         return $quote->getId() !== null;
     }
-
-    /**
-     * @return array{
-     *     bold: array{
-     *         epsAuthToken: string,
-     *         configurationGroupLabel: string,
-     *         epsUrl: string,
-     *         epsStaticUrl: string,
-     *         gatewayId: int,
-     *         jwtToken: string,
-     *         url: string,
-     *         shopId: string,
-     *         publicOrderId: string,
-     *         countries: array{
-     *             is_region_visible: bool,
-     *             label: string,
-     *             value: string
-     *         },
-     *         origin: string,
-     *         epsUrl: string,
-     *         shopUrl: string,
-     *         shopName: string,
-     *         isPhoneRequired: bool,
-     *         isExpressPayEnabled: bool,
-     *         isCartWalletPayEnabled: bool,
-     *         paymentBooster: array{
-     *             payment: object{
-     *                 method: string
-     *             }
-     *         }
-     *     }
-     * }
-     * @throws \Magento\Framework\Exception\LocalizedException
-     */
-    public function initConfig(): array
-    {
-        $this->checkoutData->initCheckoutData();
-        return $this->paymentBoosterConfigProvider->getConfig();
-    }
 }

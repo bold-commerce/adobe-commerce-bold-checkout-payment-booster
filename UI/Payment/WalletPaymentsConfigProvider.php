648ac22eb0e0984e5a44c50e3caad7f6628c35d8
Zhining Liu <zhining.liu@boldcommerce.com>
2025-01-07 09:15:12 -0800

    EPS-169: Add a new payment option for wallet pay

---
+++ UI/Payment/WalletPaymentsConfigProvider.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Bold\CheckoutPaymentBooster\UI\Payment;
+
+use Bold\CheckoutPaymentBooster\Model\Payment\Gateway\Service;
+use Magento\Checkout\Model\ConfigProviderInterface;
+
+/**
+ * Config provider for Bold Wallet Payments.
+ */
+class WalletPaymentsConfigProvider implements ConfigProviderInterface
+{
+    /**
+     * @inheirtDoc
+     */
+    public function getConfig(): array
+    {
+        return [
+            'bold' => [
+                'walletPayments' => [
+                    'payment' => [
+                        'method' => Service::CODE_WALLET,
+                    ],
+                ],
+            ],
+        ];
+    }
+}

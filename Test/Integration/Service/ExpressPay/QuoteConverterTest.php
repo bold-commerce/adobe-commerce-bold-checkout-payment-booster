47bc521ad0539a4f4b33256b368957b1626201bf
NickolasMalovanets <149711420+NickolasMalovanets@users.noreply.github.com>
2025-02-14 01:16:53 +0200

    CHK-7114: Support compatibility with Multi-currency extensions (#195)
    * Support compatibility with Multi-currency related extensions on Magento 2 with Bold Booster 2.
    
    * Code cleanup.
    
    * Restore original test for quote conversion logic (CHK-7114)
    
    * Restore test for non-base currency quote conversion (CHK-7114)
    
    * Use Euro as currency for conversion test (CHK-7114)
    
    Aligns test with tests in core integration testing framework.
    
    ---------
    
    Co-authored-by: Joseph Leedy <joseph.leedy@boldcommerce.com>

--- Test/Integration/Service/ExpressPay/QuoteConverterTest.php
+++ Test/Integration/Service/ExpressPay/QuoteConverterTest.php
@@ -6,9 +6,11 @@ namespace Bold\CheckoutPaymentBooster\Test\Integration\Service\ExpressPay;
 
 use Bold\CheckoutPaymentBooster\Service\ExpressPay\QuoteConverter;
 use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Api\SearchCriteriaInterface;
 use Magento\Framework\ObjectManagerInterface;
 use Magento\Framework\Registry;
 use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\Quote\Api\Data\CartInterface;
 use Magento\Quote\Model\Quote;
 use Magento\Quote\Model\Quote\Address\Rate;
 use Magento\Quote\Model\Quote\Address\RateResult\Error as AddressRateResultError;
@@ -223,6 +225,103 @@ class QuoteConverterTest extends TestCase
                 'a31a8fd6-a9e2-4c68-a834-54567bfeb4b7'
             )
         );
+
+        self::assertEquals($expectedConvertedQuoteData, $actualConvertedQuoteData);
+    }
+
+    /**
+     * @magentoConfigFixture current_store currency/options/base USD
+     * @magentoConfigFixture current_store currency/options/default USD
+     * @magentoConfigFixture current_store currency/options/allow USD,EUR
+     * @magentoDataFixture Bold_CheckoutPaymentBooster::Test/Integration/_files/quote_non_base_currency.php
+     */
+    public function testConvertFullQuoteConvertsNonBaseCurrencyQuote(): void
+    {
+        $objectManager = Bootstrap::getObjectManager();
+        /** @var SearchCriteriaInterface $searchCriteria */
+        $searchCriteria = $objectManager->create(SearchCriteriaBuilder::class)
+            ->addFilter('reserved_order_id', 'test_order_1')
+            ->create();
+        /** @var CartInterface[] $searchResult */
+        $searchResult = $objectManager->create(CartRepositoryInterface::class)
+            ->getList($searchCriteria)
+            ->getItems();
+        /** @var Quote $quote */
+        $quote = reset($searchResult);
+        /** @var QuoteConverter $quoteConverter */
+        $quoteConverter = $objectManager->create(QuoteConverter::class);
+
+        $expectedConvertedQuoteData = [
+            'gateway_id' => 'test-gateway-id',
+            'order_data' => [
+                'locale' => 'en-US',
+                'customer' => [
+                    'first_name' => 'John',
+                    'last_name' => 'Smith',
+                    'email' => 'customer@example.com',
+                ],
+                'shipping_address' => [
+                    'first_name' => 'John',
+                    'last_name' => 'Smith',
+                    'address_line_1' => 'Green str, 67',
+                    'address_line_2' => '',
+                    'city' => 'CityM',
+                    'country_code' => 'US',
+                    'postal_code' => '75477',
+                    'state' => 'Alabama',
+                    'phone_number' => '3468676',
+                ],
+                'selected_shipping_option' => [
+                    'id' => 'flatrate_flatrate',
+                    'label' => 'Flat Rate - Fixed',
+                    'type' => 'SHIPPING',
+                    'amount' => [
+                        'currency_code' => 'EUR',
+                        'value' => '7.07',
+                    ],
+                ],
+                'shipping_options' => [
+                    [
+                        'id' => 'flatrate_flatrate',
+                        'label' => 'Flat Rate - Fixed',
+                        'type' => 'SHIPPING',
+                        'amount' => [
+                            'currency_code' => 'EUR',
+                            'value' => '7.07',
+                        ],
+                    ],
+                ],
+                'items' => [
+                    [
+                        'name' => 'Simple Product',
+                        'sku' => 'simple',
+                        'unit_amount' => [
+                            'currency_code' => 'EUR',
+                            'value' => '7.07',
+                        ],
+                        'quantity' => 2,
+                        'is_shipping_required' => true,
+                    ],
+                ],
+                'amount' => [
+                    'currency_code' => 'EUR',
+                    'value' => '18.74',
+                ],
+                'item_total' => [
+                    'currency_code' => 'EUR',
+                    'value' => '14.14',
+                ],
+                'tax_total' => [
+                    'currency_code' => 'EUR',
+                    'value' => '1.06',
+                ],
+                'discount' => [
+                    'currency_code' => 'EUR',
+                    'value' => '3.53',
+                ],
+            ],
+        ];
+        $actualConvertedQuoteData = $quoteConverter->convertFullQuote($quote, 'test-gateway-id');
 
         self::assertEquals($expectedConvertedQuoteData, $actualConvertedQuoteData);
     }
---
+++ Test/Integration/_files/quote_non_base_currency.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Quote\Model\QuoteFactory;
+use Magento\Quote\Model\ResourceModel\Quote as QuoteResource;
+use Magento\TestFramework\Helper\Bootstrap;
+
+require __DIR__ . '/quote_with_shipping_tax_and_discount.php';
+
+$objectManager = Bootstrap::getObjectManager();
+/** @var QuoteFactory $quoteFactory */
+$quoteFactory = $objectManager->get(QuoteFactory::class);
+/** @var QuoteResource $quoteResource */
+$quoteResource = $objectManager->get(QuoteResource::class);
+$quote = $quoteFactory->create();
+$quoteResource->load($quote, 'test_order_1', 'reserved_order_id');
+$store = $quote->getStore();
+$store->unsetData('current_currency');
+$store->setCurrentCurrencyCode('EUR');
+$quote->setBaseCurrencyCode('USD');
+$quote->setQuoteCurrencyCode('EUR');
+$quote->save();

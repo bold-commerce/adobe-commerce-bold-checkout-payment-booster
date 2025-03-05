93b5b2c8bdfb7cc7212ca2b21799340381ff9a72
Joseph Leedy <joseph.leedy@boldcommerce.com>
2025-03-05 13:54:24 -0700

    Fix Digital Wallets errors for logged-in customers (CHK-7953)
    Restores logic for setting customer data and authentication state in
    the browser's `window` object removed by commit f268c4a6.

--- ViewModel/ExpressPay.php
+++ ViewModel/ExpressPay.php
@@ -8,6 +8,14 @@ use Bold\CheckoutPaymentBooster\Model\CheckoutData;
 use Exception;
 use Magento\Checkout\Model\CompositeConfigProvider;
 use Magento\Checkout\Model\Session;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Api\Data\CustomerInterface;
+use Magento\Customer\Model\Address\CustomerAddressDataProvider;
+use Magento\Customer\Model\Context as CustomerContext;
+use Magento\Customer\Model\Data\Customer;
+use Magento\Framework\App\Http\Context as HttpContext;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Psr\Log\LoggerInterface;
 
@@ -34,21 +42,44 @@ class ExpressPay implements ArgumentInterface
     private $logger;
 
     /**
+     * @var HttpContext
+     */
+    private $httpContext;
+
+    /**
+     * @var CustomerRepositoryInterface
+     */
+    private $customerRepository;
+    /**
+     * @var CustomerAddressDataProvider
+     */
+    private $customerAddressDataProvider;
+
+    /**
      * @param CompositeConfigProvider $configProvider
      * @param Session $checkoutSession
      * @param CheckoutData $checkoutData
      * @param LoggerInterface $logger
+     * @param HttpContext $httpContext
+     * @param CustomerRepositoryInterface $customerRepository
+     * @param CustomerAddressDataProvider $customerAddressDataProvider
      */
     public function __construct(
         CompositeConfigProvider $configProvider,
         Session $checkoutSession,
         CheckoutData $checkoutData,
-        LoggerInterface $logger
+        LoggerInterface $logger,
+        HttpContext $httpContext,
+        CustomerRepositoryInterface $customerRepository,
+        CustomerAddressDataProvider $customerAddressDataProvider
     ) {
         $this->configProvider = $configProvider;
         $this->checkoutSession = $checkoutSession;
         $this->checkoutData = $checkoutData;
         $this->logger = $logger;
+        $this->httpContext = $httpContext;
+        $this->customerRepository = $customerRepository;
+        $this->customerAddressDataProvider = $customerAddressDataProvider;
     }
 
     /**
@@ -69,5 +100,51 @@ class ExpressPay implements ArgumentInterface
             $this->logger->error('ExpressPay: ' . $e->getMessage());
             return [];
         }
+    }
+
+    public function isCustomerLoggedIn(): bool
+    {
+        return (bool)$this->httpContext->getValue(CustomerContext::CONTEXT_AUTH);
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function getCustomerData(): array
+    {
+        $customerData = [];
+
+        if (!$this->isCustomerLoggedIn()) {
+            return $customerData;
+        }
+
+        try {
+            /** @var CustomerInterface&Customer $customer */
+            $customer = $this->getCustomer();
+        } catch (LocalizedException $localizedException) {
+            return $customerData;
+        }
+
+        $customerData = $customer->__toArray();
+
+        try {
+            $customerData['addresses'] = $this->customerAddressDataProvider->getAddressDataByCustomer($customer);
+        } catch (LocalizedException $e) {
+            $customerData['addresses'] = [];
+        }
+
+        return $customerData;
+    }
+
+    /**
+     * @throws NoSuchEntityException
+     * @throws LocalizedException
+     */
+    private function getCustomer(): CustomerInterface
+    {
+        /** @var int $customerId */
+        $customerId = $this->httpContext->getValue('customer_id');
+
+        return $this->customerRepository->getById($customerId);
     }
 }

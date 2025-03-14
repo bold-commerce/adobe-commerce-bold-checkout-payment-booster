ee3ef09979d5537ac8b99f487bd8365726bee6bd
Joseph Leedy <joseph.leedy@boldcommerce.com>
2025-03-14 15:46:50 -0600

    Save Cron expression after config is saved (CHK-7757)

--- Model/Config/Backend/DigitalWallets/DeactivateQuotes.php
+++ Model/Config/Backend/DigitalWallets/DeactivateQuotes.php
@@ -4,13 +4,54 @@ declare(strict_types=1);
 
 namespace Bold\CheckoutPaymentBooster\Model\Config\Backend\DigitalWallets;
 
+use Exception;
+use Magento\Framework\App\Cache\TypeListInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\App\Config\Value;
+use Magento\Framework\App\Config\ValueFactory;
+use Magento\Framework\Data\Collection\AbstractDb;
 use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Model\Context;
+use Magento\Framework\Model\ResourceModel\AbstractResource;
+use Magento\Framework\Registry;
 
 use function __;
 
 class DeactivateQuotes extends Value
 {
+    public const CRON_EXPRESSION_PATH =
+        'crontab/default/jobs/bold_booster_deactivate_digital_wallets_quotes/schedule/cron_expression';
+
+    /**
+     * @var ValueFactory
+     */
+    private $valueFactory;
+
+    /**
+     * @param Context $context
+     * @param Registry $registry
+     * @param ScopeConfigInterface $config
+     * @param TypeListInterface $cacheTypeList
+     * @param ValueFactory $valueFactory
+     * @param AbstractResource|null $resource
+     * @param AbstractDb|null $resourceCollection
+     * @param mixed[] $data
+     */
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        ScopeConfigInterface $config,
+        TypeListInterface $cacheTypeList,
+        ValueFactory $valueFactory,
+        AbstractResource $resource = null,
+        AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        $this->valueFactory = $valueFactory;
+
+        parent::__construct($context, $registry, $config, $cacheTypeList, $resource, $resourceCollection, $data);
+    }
+
     public function beforeSave(): self
     {
         $frequency = $this->getValue();
@@ -27,6 +68,39 @@ class DeactivateQuotes extends Value
         return parent::beforeSave();
     }
 
+    public function afterSave(): self
+    {
+        $frequency = $this->getValue();
+        $occurrence = $this->getOccurrence();
+        $sequence = $this->getSequence();
+        $cronExpression = '';
+
+        if ($frequency === 'hourly') {
+            $cronExpression = "0 */$occurrence * * *";
+        }
+
+        if ($frequency === 'daily') {
+            $cronExpression = "0 0 */$occurrence * *";
+        }
+
+        if ($frequency === 'custom') {
+            $cronExpression = $sequence;
+        }
+
+        try {
+            // @phpstan-ignore-next-line
+            $this->valueFactory->create()
+                ->load(self::CRON_EXPRESSION_PATH, 'path')
+                ->setValue($cronExpression)
+                ->setPath(self::CRON_EXPRESSION_PATH)
+                ->save();
+        } catch (Exception $exception) {
+            throw new LocalizedException(__('Could not save the Cron expression.'), $exception);
+        }
+
+        return parent::afterSave();
+    }
+
     private function getOccurrence(): int
     {
         if ($this->getFieldsetDataValue('occurrence') !== null) {
@@ -38,6 +112,20 @@ class DeactivateQuotes extends Value
             $this ->_config->getValue(
                 'checkout/bold_checkout_payment_booster_advanced/digital_wallets_quote_cleanup_occurrence'
             ) ?? 1
+        );
+    }
+
+    private function getSequence(): string
+    {
+        if ($this->getFieldsetDataValue('sequence') !== null) {
+            return $this->getFieldsetDataValue('sequence');
+        }
+
+        // @phpstan-ignore-next-line
+        return (string)(
+            $this ->_config->getValue(
+                'checkout/bold_checkout_payment_booster_advanced/digital_wallets_quote_cleanup_sequence'
+            ) ?? '* * * * *'
         );
     }
 }

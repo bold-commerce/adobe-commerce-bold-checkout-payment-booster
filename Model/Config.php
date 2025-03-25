2e5c8e49777124652b7d7f12d453ad2cf972a7a7
Joseph Leedy <joseph.leedy@boldcommerce.com>
2025-03-25 11:48:30 -0600

    Fix "Call to an undefined method" warning (CHK-8121)
    Fixes warning reported during static analysis.

--- Model/Config.php
+++ Model/Config.php
@@ -38,7 +38,7 @@ class Config
     private const PATH_IS_TAX_INCLUDED_IN_SHIPPING = 'tax/calculation/shipping_includes_tax';
 
     /**
-     * @var ScopeConfigInterface
+     * @var ScopeConfigInterface&\Magento\Framework\App\Config
      */
     private $scopeConfig;
 
@@ -58,7 +58,7 @@ class Config
     private $encryptor;
 
     /**
-     * @param ScopeConfigInterface $scopeConfig
+     * @param ScopeConfigInterface&\Magento\Framework\App\Config $scopeConfig
      * @param WriterInterface $configWriter
      * @param TypeListInterface $cacheTypeList
      * @param EncryptorInterface $encryptor

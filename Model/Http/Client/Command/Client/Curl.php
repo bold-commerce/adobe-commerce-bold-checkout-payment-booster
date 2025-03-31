83a922104723241ee601d2edeccce402a6effec8
Joseph Leedy <joseph.leedy@boldcommerce.com>
2025-03-31 09:45:19 -0600

    Remove unnecessary qualifier from method annotation (CHK-8121)

--- Model/Http/Client/Command/Client/Curl.php
+++ Model/Http/Client/Command/Client/Curl.php
@@ -8,7 +8,7 @@ use Magento\Framework\HTTP\Client\Curl as CurlCore;
 /**
  * Bold Checkout Curl client.
  *
- * @method \void curlOption(int $option, mixed $value)
+ * @method void curlOption(int $option, mixed $value)
  */
 class Curl extends CurlCore
 {

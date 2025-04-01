daff15798950784209c865e92d6837baa57ee495
Joseph Leedy <joseph.leedy@boldcommerce.com>
2025-04-01 15:25:35 -0600

    Declare class constant visibility in Onboard Banner (CHK-8120)

--- Block/System/Config/Form/Field/OnboardBanner.php
+++ Block/System/Config/Form/Field/OnboardBanner.php
@@ -13,8 +13,8 @@ use PHPUnit\Util\Exception;
 
 class OnboardBanner extends Field
 {
-    const ONBOARD_IN_PROGRESS_DATA_PATH = 'https://apps.boldapps.net/onboarding_banner/adobe-commerce/payment-booster/in_progress';
-    const ONBOARD_COMPLETED_DATA_PATH = 'https://apps.boldapps.net/onboarding_banner/adobe-commerce/payment-booster/complete';
+    private const ONBOARD_IN_PROGRESS_DATA_PATH = 'https://apps.boldapps.net/onboarding_banner/adobe-commerce/payment-booster/in_progress';
+    private const ONBOARD_COMPLETED_DATA_PATH = 'https://apps.boldapps.net/onboarding_banner/adobe-commerce/payment-booster/complete';
 
     /** @var StoreManagerInterface */
     private $storeManager;

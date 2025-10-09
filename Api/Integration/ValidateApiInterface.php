ef78c936c8c83a22b8b40cc9fca476c519747ed2
Felipe dos Santos <felipe.dossantos@boldcommerce.com>
2025-10-09 16:12:14 -0600

    CHK-9310: Update Integration validate endpoint

--- Api/Integration/ValidateApiInterface.php
+++ Api/Integration/ValidateApiInterface.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Bold\CheckoutPaymentBooster\Api\Integration;
 
-use Bold\CheckoutPaymentBooster\Api\Data\Http\Client\ResultInterface;
-
 /**
  * Validate Bold Checkout API Integration.
  */
@@ -15,9 +13,9 @@ interface ValidateApiInterface
      * Validate Bold Checkout API Integration.
      *
      * @param string $shopId
-     * @return \Bold\CheckoutPaymentBooster\Api\Data\Http\Client\ResultInterface
+     * @return \Bold\CheckoutPaymentBooster\Api\Data\Integration\IntegrationResultInterface
      */
     public function validate(
         string $shopId,
-    ): ResultInterface;
+    ): \Bold\CheckoutPaymentBooster\Api\Data\Integration\IntegrationResultInterface;
 }

096c7ef667503a8f5c402a08aa750a14bdea3c2d
Carol Castro <carol.castro@boldcommerce.com>
2026-06-02 15:36:00 -0300

    Magento 2.4.9 compatibility

--- Test/Integration/Service/DigitalWallets/MagentoQuote/CreatorTest.php
+++ Test/Integration/Service/DigitalWallets/MagentoQuote/CreatorTest.php
@@ -511,6 +511,7 @@ class CreatorTest extends TestCase
             && $this->getMagentoVersion() != '2.4.8-p1'
             && $this->getMagentoVersion() != '2.4.9-alpha2'
             && $this->getMagentoVersion() != '2.4.9-alpha3'
+            && $this->getMagentoVersion() != '2.4.9'
         ) {
             $quoteRepositoryMock
                 ->expects($matcher)

2665a27688274af7cbb4c7da4b17caf07467b260
Carol Castro <carol.castro@boldcommerce.com>
2026-07-10 12:47:34 -0300

    Fix integration test

--- Test/Integration/Plugin/Booster/Service/DigitalWallets/MagentoQuote/CreatorPluginTest.php
+++ Test/Integration/Plugin/Booster/Service/DigitalWallets/MagentoQuote/CreatorPluginTest.php
@@ -176,9 +176,12 @@ class CreatorPluginTest extends IntegrationTestCase
     /**
      * After order_complete clears bold_order_id, a stale client ID must still trigger re-init.
      *
+     * order.php runs default_rollback (deletes catalog); reload virtual product after it.
+     *
      * @magentoAppArea frontend
      * @magentoDataFixture Bold_CheckoutPaymentBooster::Test/Integration/_files/magento_quote_bold_order.php
      * @magentoDataFixture Magento/Sales/_files/order.php
+     * @magentoDataFixture Magento/Catalog/_files/product_virtual.php
      */
     public function testCreatorPluginRejectsClearedButCompletedPublicOrderId(): void
     {
@@ -215,7 +218,7 @@ class CreatorPluginTest extends IntegrationTestCase
         $storeManager = $objectManager->get(StoreManagerInterface::class);
         /** @var ProductRepositoryInterface $productRepository */
         $productRepository = $objectManager->create(ProductRepositoryInterface::class);
-        $product = $productRepository->get('simple');
+        $product = $productRepository->get('virtual-product');
         /** @var Creator $magentoQuoteCreator */
         $magentoQuoteCreator = $objectManager->create(Creator::class);
 
